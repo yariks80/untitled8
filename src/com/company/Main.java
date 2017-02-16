bab4e3757ad0bd13e57c13819b728f52cc2b8eb3
Iaroslav Sviatkin <yariks@i.ua>
2017-02-16 11:22:40 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+/**
+ * Created by Administrator on 2/16/2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hi");
+
+    }
+}
